666b4087713438b61739c6ab8a879a2c0ecf6405
Gurjit Singh <33502425+CloudHint@users.noreply.github.com>
2020-02-02 06:31:24 -0800

    Added the full path to .bash_profile

--- color.sh
+++ color.sh
@@ -4,9 +4,9 @@
 cp ~/.bash_profile ~/.bash_profile.backup
 
 # copy and paste the following in the .bash_profile and save
-echo 'export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]\$"' >> .bash_profile
-echo "export CLICOLOR=1" >> .bash_profile
-echo "export LSCOLORS=ExFxBxDxCxegedabagacad" >> .bash_profile
+echo 'export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]\$"' >> ~/.bash_profile
+echo "export CLICOLOR=1" >> ~/.bash_profile
+echo "export LSCOLORS=ExFxBxDxCxegedabagacad" >> ~/.bash_profile
 
 # restart the .bash_profil e
 . ~/.bash_profile
