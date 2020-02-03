b321426176411601d7bac8cbee99e4da9f1b5280
Gurjit Singh <33502425+CloudHint@users.noreply.github.com>
2020-02-02 21:31:27 -0800

    #ed the chmod line

--- color.sh
+++ color.sh
@@ -5,7 +5,7 @@ DEST=~/.bash_profile
 # copy the .bash_profile to .bash_profile.backup
 cp $DEST ~/.bash_profile.backup
 # make .bash_profile executable
-chmod o+x ~/.bash_profile
+#chmod o+x $DEST 
 
 # copy and paste the following in the .bash_profile and save
 echo 'export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]\$"' >> $DEST
